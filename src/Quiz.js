8067dbdd39df299df02fedb0d7c965675e9d7609
Jaclyn Ong <jaclyn.ong@liferay.com>
2019-11-25 11:12:34 -0800

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -24,10 +24,17 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
     );
+  }
+
+  showNextQuestion() {
+    this.setState(state => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
   }
 }
 
